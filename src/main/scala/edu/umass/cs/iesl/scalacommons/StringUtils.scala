5636a65d29773cb1e961f287d04fe0a965bc550b
David Soergel <dev@davidsoergel.com>
2012-03-27 14:21:27 -0400

    String cleanup utils

--- src/main/scala/edu/umass/cs/iesl/scalacommons/StringUtils.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/StringUtils.scala
@@ -6,5 +6,12 @@ object StringUtils {
 
   implicit def stringToOptionInt(s: String): Option[Int] = if (s.trim.isEmpty) None else Some(s.toInt)
 
+  implicit def enrichString(s: String) = new RichString(s)
+}
 
+class RichString(val s: String) {
+  def removeNewlines: String = s.replaceAll("[\\n\\r]", " ")
+  def removePunctuation: String = s.replaceAll("\\p{Punct}", " ")
+  def removeAllButWord: String = s.replaceAll("[^\\w\\s]", " ")
+  def collapseWhitespace: String = s.replaceAll("\\s", " ")
 }
