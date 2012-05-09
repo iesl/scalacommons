06b2d5ffb4fbfaf0b72d983087c0bf3197295a90
David Soergel <dev@davidsoergel.com>
2012-05-09 16:51:46 -0400

    Mostly commented out brainstorms of various PrioritySet things

--- src/main/scala/edu/umass/cs/iesl/scalacommons/Memoize.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/Memoize.scala
@@ -1,35 +1,90 @@
 package edu.umass.cs.iesl.scalacommons
 
-
 //http://michid.wordpress.com/2009/02/23/function_mem/
-class Memoize1[-T, +R](f: T => R) extends (T => R) {
+class Memoize1[-T, +R](f: T => R) extends (T => R)
+	{
 
-  import scala.collection.mutable
+	import scala.collection.mutable
 
-  private[this] val vals = mutable.Map.empty[T, R]
+	protected[this] val vals = mutable.Map.empty[T, R]
 
-  def apply(x: T): R = {
-    if (vals.contains(x)) {
-      vals(x)
-    }
-    else {
-      val y = f(x)
-      vals += ((x, y))
-      y
-    }
-  }
-}
+	def apply(x: T): R =
+		{
+		if (vals.contains(x))
+			{
+			vals(x)
+			}
+		else
+			{
+			val y = f(x)
+			vals += ((x, y))
+			y
+			}
+		}
+	}
 
-object Memoize1 {
-  def apply[T, R](f: T => R) = new Memoize1(f)
+object Memoize1
+	{
+	def apply[T, R](f: T => R) = new Memoize1(f)
 
-  def Y[T, R](f: (T, T => R) => R) = {
-    var yf: T => R = null
-    yf = Memoize1(f(_, yf(_)))
-    yf
-  }
-}
+	def Y[T, R](f: (T, T => R) => R) =
+		{
+		var yf: T => R = null
+		yf = Memoize1(f(_, yf(_)))
+		yf
+		}
+	}
 
+class InvalidatableMemoize1[-T, +R](f: T => R) extends Memoize1[T, R](f)
+	{
+	def remove(x: T) = vals.remove(x)
+
+	def clear() { vals.clear() }
+	}
+
+object InvalidatableMemoize1
+	{
+	def apply[T, R](f: T => R) = new InvalidatableMemoize1(f)
+	}
+/*
+class ConditionalMemoize1[-T, +R](f: T => R, condition: R => Boolean) extends (T => R)
+	{
+
+	import scala.collection.mutable
+
+	protected[this] val vals = mutable.Map.empty[T, R]
+
+	def apply(x: T): Option[R] =
+		{
+		if (vals.contains(x))
+			{
+			Some(vals(x))
+			}
+		else
+			{
+			val y = f(x)
+			if (condition(y))
+				{
+				vals += ((x, y))
+				Some(y)
+				}
+			else None
+			}
+		}
+	}
+
+object ConditionalMemoize1
+	{
+	def apply[T, R](f: T => R, condition: R => Boolean) = new ConditionalMemoize1(f, condition)
+
+	def Y[T, R](f: (T, T => R) => R) =
+		{
+		var yf: T => R = null
+		yf = Memoize1(f(_, yf(_)))
+		yf
+		}
+	}
+*/
 /*
 case class Memoize1[-T, +R](f: T => R) extends Function1[T, R]
   {
@@ -44,9 +99,9 @@ case class Memoize1[-T, +R](f: T => R) extends Function1[T, R]
 object RecursiveMemoizedFunction
   {
   def apply[T, R](fRec: (T, T => R) => R): (T => R) =
-    {
-    def f(n: T): R = fRec(n, n => f(n))
-    Memoize1(f)
-    }
+	{
+	def f(n: T): R = fRec(n, n => f(n))
+	Memoize1(f)
+	}
   }
 */
--- src/main/scala/edu/umass/cs/iesl/scalacommons/collections/PrioritySet.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/collections/PrioritySet.scala
@@ -1,15 +1,18 @@
 package edu.umass.cs.iesl.scalacommons.collections
 
-import collection.mutable.{Set, HashSet, PriorityQueue}
+import scala.annotation.tailrec
+import edu.umass.cs.iesl.scalacommons.InvalidatableMemoize1
+import scala.collection.mutable.{Set, HashSet, PriorityQueue}
+import scala._
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
  */
-class PrioritySet[A](implicit val ord: Ordering[A]) extends PriorityQueue[A]
+class PrioritySet[A](override val ord: Ordering[A]) extends PriorityQueue[A]()(ord)
 	{
 	// some other data structure might be more efficient, but the redundant solution is quick and easy for now
-	private val elementSet: Set[A] = HashSet[A]()
+	protected val elementSet: Set[A] = HashSet[A]()
 
 	override def +=(elem: A): this.type =
 		{
@@ -26,39 +29,178 @@ class PrioritySet[A](implicit val ord: Ordering[A]) extends PriorityQueue[A]
 		}
 	}
 
+/*
+trait ConditionalPrioritySet[A] extends PrioritySet[A]
+	{
+	def condition(a: A): Boolean
+
+	override def +=(elem: A): this.type =
+		{
+		if (condition(elem))
+			{super.+=(elem)}
+		else this
+		}
+	}
+*/
 /**
- * Instead of an ordering, provide a function that computes a comparable priority value, and cache that
+ * Instead of an ordering, provide a function that computes a comparable priority value.  Priority values must remain stable,
+ * so we cache them internally.
  */
-trait ComputedPrioritySet[A] extends PrioritySet[A]
+class MemorizedPriorityOrdering[A, P <: Ordered[P]](priority: (A) => P) extends Ordering[A]
+	{
+	val mpriority = InvalidatableMemoize1(priority)
+
+	def compare(x: A, y: A) = mpriority(x).compare(mpriority(y))
+	}
+
+// this just serves to make the mpo available to subclasses
+class MemorizedPrioritySet[A, P <: Ordered[P]](val mpo: MemorizedPriorityOrdering[A, P]) extends PrioritySet[A](mpo)
+
+class ComputedPrioritySet[A, P <: Ordered[P]](priority: (A => P)) extends MemorizedPrioritySet[A, P](new MemorizedPriorityOrdering(priority))
+	{
+	override def dequeue(): A =
+		{
+		val a = super.dequeue()
+		mpo.mpriority.remove(a) // no need to remember priorities for dequeued items
+		a
+		}
+	}
+
+/**
+ * If the priority for a given item may become invalid, but the only alterations allowed to priorities decrease them,
+ * then we can wait to recompute an element priority until we see it.
+ * Caching is managed internally, so don't pass a memoized priority function!
+ */
+class LazyRecomputingPrioritySet[A, P <: Ordered[P]](priority: (A => P), priorityIsValid: (A => Boolean))
+		extends MemorizedPrioritySet[A, P](new MemorizedPriorityOrdering(priority))
+	{
+	@tailrec
+	override final def dequeue(): A =
+		{
+		val a = super.dequeue()
+
+		val p = mpo.mpriority.remove(a) // always forget a cached priority upon dequeuing, since it is either invalid or no longer needed anyway
+
+		if (priorityIsValid(a))
+			{
+			a
+			}
+		else
+			{
+			// re-enqueue this item; its priority was just forgotten so it will be automatically recomputed
+			this += a
+
+			// take the new best item
+			dequeue()
+			}
+		}
+	}
+
+/*
+/**
+ * A cache for priority values which keeps track of the "time" when a priority was computed, so as to tell whether a value is up to date.
+ */
+class GenerationalPriorityOrdering[A, P <: Ordered[P]](val priority: (A) => P) extends Ordering[A]
+	{
+	var currentGeneration: Int = 0
+
+	def increment = currentGeneration += 1
+
+	val validInGenerations = mutable.Map[A, Int]()
+
+	// record the generation at which each priority is computed
+	def vpriority(a: A): P =
+		{
+		validInGenerations.put(a, currentGeneration)
+		priority(a)
+		}
+
+	val mpriority = InvalidatableMemoize1(vpriority)
+
+	def remove(a: A) = mpriority.remove(a)
+
+	def compare(x: A, y: A) = mpriority(x).compare(mpriority(y))
+
+	def priorityIsCurrent(a: A) =
+		{validInGenerations(a) == currentGeneration}
+	}
+
+object GenerationalRecomputingPrioritySet
+	{
+	def apply[A, P <: Ordered[P]](priority: (A => P)) =
+		{
+		val ord = new GenerationalPriorityOrdering[A, P](priority)
+		new GenerationalRecomputingPrioritySet[A, P](ord)
+		}
+	}
+
+object GenerationalRecomputingPrioritySetWithLowerBound
 	{
 
+	def apply[A, P <: Ordered[P]](priority: (A => Option[P])) =
+		{
+		val ord = new GenerationalPriorityOrdering[A, P](priority)
+		new GenerationalRecomputingPrioritySet[A, P](ord)
+		}
 	}
+
+/**
+ * A LazyRecomputingPrioritySet where old priorities may become invalid only through the addition of new items to the set
+ */
+class GenerationalRecomputingPrioritySet[A, P <: Ordered[P]](ord: GenerationalPriorityOrdering[A, P])
+		extends LazyRecomputingPrioritySet(ord.priority, ord.priorityIsCurrent)
+	{
+	override def +=(elem: A): this.type =
+		{
+		val result = super.+=(elem)
+		if (result != this) ord.increment
+		result
+		}
+
+	@tailrec
+	override def dequeue(): A =
+		{
+		val a = super.dequeue()
+		ord.remove(a) // always forget a cached priority upon dequeuing, since it is either invalid or no longer needed anyway
+
+		if (ord.priorityIsCurrent(a))
+			{
+			a
+			}
+		else
+			{
+			// re-enqueue this item; its priority was just forgotten so it will be automatically recomputed
+			this += a
+
+			// take the new best item
+			dequeue()
+			}
+		}
+	}
+*/
 /**
  * A PrioritySet with the property that adding or removing an element may alter the priorities of some of the _other_ elements.
  */
-abstract trait DynamicPrioritySet[A] extends PrioritySet[A]
-
+//abstract trait DynamicPrioritySet[A] extends PrioritySet[A]
 /**
  * When some of the other elements are affected, immediately recompute their qualities and requeue them
+ *
+ * actually we can't do this easily because PriorityQueue does not provide remove or -=.
  */
-trait EagerDynamicPrioritySet[A] extends DynamicPrioritySet[A]
+/*
+class EagerDynamicPrioritySet[A](val isAffectedBy : ((A,A)=>Boolean)) extends DynamicPrioritySet[A]
 	{
 
 	override def +=(elem: A): this.type =
 		{
-
 		val otherElems = elementSet.filter(isAffectedBy(_, elem))
 		for (o <- otherElems)
 			{
-			remove(o)
+			this -= (o)  // **
 			super.enqueue(o) // the implicit ordering will kick in and possibly return a different result this time
 			}
 
 		super.+=(elem)
 		}
-}
-
-	/**
-	 * If the only alterations allowed to priorities decrease them, then we can wait to recompute an element priority until we see it
-	 */
-	class LazyDecreasingDynamicPrioritySet[A] extends DynamicPrioritySet[A]
+	}
+*/
