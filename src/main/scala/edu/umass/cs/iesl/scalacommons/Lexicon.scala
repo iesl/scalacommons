95a5adfa4b9be62391fb48bb77e95a0273bc12c8
David Soergel <dev@davidsoergel.com>
2012-08-30 15:44:42 -0400

    add generic argMax, improve Lexicon API

--- src/main/scala/edu/umass/cs/iesl/scalacommons/Lexicon.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/Lexicon.scala
@@ -1,6 +1,9 @@
 package edu.umass.cs.iesl.scalacommons
 
 import java.io.InputStream
+import StringUtils._
+import com.weiglewilczek.slf4s.Logging
+import scala.util.matching.Regex
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
@@ -12,36 +15,60 @@ import java.io.InputStream
 // or
 // val stopwords = new Lexicon(getClass.getResourceAsStream("/lexicons/stopwords"))
 
-class Lexicon(s: InputStream) {
+class Lexicon(s: InputStream) extends Logging {
 
   def this(name: String) = this(getClass.getResourceAsStream("/lexicons/" + name))
 
-  val (lexTokens, lexTokensLC) = {
+  private val (lexTokens, lexTokensLC, lexTokensStrippedLCREs) = {
     val text: String = IOUtils.loadText(s)
-    val lexTokensLC: Map[String, Boolean] = text.toLowerCase.split("\n").map(x => (x -> true)).toMap
-    val lexTokens: Map[String, Boolean] = text.split("\n").map(x => (x -> true)).toMap
-    (lexTokens, lexTokensLC)
+    val lexTokens: Set[String] = text.split("\n").toSet
+    for (t <- lexTokens if t.contains(" ")) {
+      logger.warn("Token contains a space: " + t + ", use countSubstringMatchesLC")
+    }
+    val lexTokensLC: Set[String] = lexTokens.map(_.toLowerCase)
+    val lexTokensStrippedLCREs: Set[Regex] = lexTokensLC.map(_.stripPunctuation.r)
+    (lexTokens, lexTokensLC, lexTokensStrippedLCREs)
   }
 
   def countMatches(tokens: Seq[String]): Int = {
-    tokens.map(lexTokens.get(_)).flatten.length
+    tokens.filter(lexTokens.contains(_)).flatten.length
   }
 
   def countMatchesLC(tokens: Seq[String]): Int = {
-    tokens.map((x: String) => lexTokensLC.get(x.toLowerCase)).flatten.length
+    tokens.filter(s => lexTokensLC.contains(s.toLowerCase)).flatten.length
   }
 
+  // Maybe: consider newlines in the input to be a hard constraint, but strip the spaces within each line
+  def countSubstringMatchesLC(s: String): Int = substringMatchesLC(s).values.sum
+
+  /* {
+    val target = s.toLowerCase
+    (for (r <- lexTokensStrippedLCREs) yield r.findAllIn(target).size).sum
+  }*/
+
+  def substringMatchesLC(s: String): Map[String, Int] = {
+    val target = s.toLowerCase
+    val matchesPerToken = (for (r <- lexTokensStrippedLCREs) yield (r.toString, r.findAllIn(target).size)).filter(_._2 > 0).toMap
+    matchesPerToken
+  }
+
+
+  // use these as filters, e.g. mySeq.filter(someLexicon.exact)
+
+  def exact(s: String): Boolean = lexTokens.contains(s)
+
+  def lc(s: String): Boolean = lexTokensLC.contains(s)
+
+  /*
   def matches(tokens: Seq[String]): Seq[String] = {
-    tokens.filter(t => {
-      val r: Option[Boolean] = lexTokens.get(t)
-      r.getOrElse(false)
-    })
+    tokens.filter(t => lexTokens.contains(t)))
   }
 
   def matchesLC(tokens: Seq[String]): Seq[String] = {
     tokens.filter(t => {
-      val r: Option[Boolean] = lexTokensLC.get(t.toLowerCase)
+      val r: Option[Boolean] = lexTokensLC.contains(t.toLowerCase)
       r.getOrElse(false)
     })
-  }
+
+  }*/
 }
--- src/main/scala/edu/umass/cs/iesl/scalacommons/SeqUtils.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/SeqUtils.scala
@@ -1,51 +1,58 @@
 package edu.umass.cs.iesl.scalacommons
 
 import com.weiglewilczek.slf4s.Logging
+import collection.GenTraversable
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
  */
-object SeqUtils extends Logging
-	{
+object SeqUtils extends Logging {
 
-	implicit def emptyCollectionToNone[T <: Iterable[Any]](s: T): Option[T] = if (s.isEmpty) None else Some(s)
+  implicit def emptyCollectionToNone[T <: GenTraversable[Any]](s: T): Option[T] = if (s.isEmpty) None else Some(s)
 
-	def mergeWarn[T](a: Seq[T], b: Seq[T]): Seq[T] =
-		{
-		(a, b) match
-		{
-			case (Nil, Nil) => Nil
-			case (p, Nil) => a
-			case (Nil, q) => b
-			case (p, q) =>
-				{
-				if (p != q)
-					{
-					logger.warn("Merging unequal sequences, preferring: " + p + "  to " + q)
-					}
-				a
-				}
-		}
-		}
+  def mergeWarn[T](a: GenTraversable[T], b: GenTraversable[T]): GenTraversable[T] = {
+    (a, b) match {
+      case (Nil, Nil) => Nil
+      case (p, Nil) => a
+      case (Nil, q) => b
+      case (p, q) => {
+        if (p != q) {
+          logger.warn("Merging unequal sequences, preferring: " + p + "  to " + q)
+        }
+        a
+      }
+    }
+  }
 
-	def mergeOrFail[T](a: Seq[T], b: Seq[T]): Seq[T] =
-		{
-		(a, b) match
-		{
-			case (Nil, Nil) => Nil
-			case (p, Nil) => a
-			case (Nil, q) => b
-			case (p, q) =>
-				{
-				if (p != q)
-					{
-					throw new SeqMergeException(p, q)
-					}
-				a
-				}
-		}
-		}
-	}
+  def mergeOrFail[T](a: GenTraversable[T], b: GenTraversable[T]): GenTraversable[T] = {
+    (a, b) match {
+      case (Nil, Nil) => Nil
+      case (p, Nil) => a
+      case (Nil, q) => b
+      case (p, q) => {
+        if (p != q) {
+          throw new SeqMergeException(p, q)
+        }
+        a
+      }
+    }
+  }
+
+
+  // based on Daniel Sobral.  http://stackoverflow.com/questions/3050557/how-can-i-extend-scala-collections-with-an-argmax-method
+  def argMax[A, B: Ordering](input: Iterable[A], f: A => B) = argMaxZip(input, f) map (_._1) toSet
+
+  // compact version
+
+  def argMaxZip[A, B: Ordering](input: Iterable[A], f: A => B): Iterable[(A, B)] = {
+    val fList = input map f
+    val maxFList = fList.max
+    input.view zip fList filter (_._2 == maxFList)
+  }
+
+  // could do a version that produces (B, Iterable[A]]
+
+}
 
 class SeqMergeException[T](x: T, y: T) extends Exception("unequal sequences: " + x + "  ,  " + y)
