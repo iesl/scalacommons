2a8c0ba7f3216c35187c1698c9f35e80e0c56f78
David Soergel <dev@davidsoergel.com>
2013-01-22 15:44:47 -0500

    Add MapUtils.invert

---
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/MapUtils.scala
@@ -0,0 +1,13 @@
+package edu.umass.cs.iesl.scalacommons
+
+/**
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ */
+object MapUtils {
+  //http://daily-scala.blogspot.com/2010/03/how-to-reverse-map.html
+  def invert[A, B](x: Map[A, B]): Map[B, Iterable[A]] = x groupBy {
+    _._2
+  } map {
+    case (key, value) => (key, value.unzip._1)
+  }
+}
