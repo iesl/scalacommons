5954e9d74c90105658681f34455586b19793c011
David Soergel <dev@davidsoergel.com>
2013-09-23 18:44:21 -0400

    add firstline

--- src/main/scala/edu/umass/cs/iesl/scalacommons/StringUtils.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/StringUtils.scala
@@ -81,6 +81,15 @@ class RichString(val s: String) extends Logging {
 
   def limit(len: Int): String = s.substring(0, math.min(s.length, len))
 
+  def firstLine = {
+    // surely there is a more idiomatic solution?
+    val i : Int = s.indexOf("\n") 
+    i match {
+      case -1 => s
+      case _ => limit(i)
+    }
+  }
+  
   def limitAtWhitespace(len: Int, suffixIfLimited: String) = {
     val l = limit(len + 1) // allow for a space after the last retained word
     if (l.length < s.length) {
