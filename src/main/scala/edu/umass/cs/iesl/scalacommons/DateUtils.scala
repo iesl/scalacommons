2fe808022623437399ac3bcbe2e857a1c6218cae
David Soergel <dev@davidsoergel.com>
2013-11-18 13:38:27 -0500

    Pretty formatting of date ranges

--- src/main/scala/edu/umass/cs/iesl/scalacommons/DateUtils.scala
+++ src/main/scala/edu/umass/cs/iesl/scalacommons/DateUtils.scala
@@ -3,6 +3,8 @@ package edu.umass.cs.iesl.scalacommons
 import collection.mutable
 import java.util.Locale
 import java.text.DateFormatSymbols
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 object DateUtils {
 
@@ -45,5 +47,35 @@ object DateUtils {
   def parseMonthZeroBased(s: String): Option[Int] = zeroBasedMonthsByName.get(s.trim)
 
   def parseMonthOneBased(s: String): Option[Int] = zeroBasedMonthsByName.get(s.trim).map(_ + 1)
+
+  def formatInterval(x: Option[DateTime], y: Option[DateTime]): String = {
+
+    val yearOnlyFormat = DateTimeFormat.forPattern("yyyy")
+    val monthAndYearFormat = DateTimeFormat.forPattern("MMM yyyy")
+    val yearformat = DateTimeFormat.forPattern("dd MMM yyyy")
+    val monthformat = DateTimeFormat.forPattern("dd MMM")
+    val dayformat = DateTimeFormat.forPattern("dd")
+
+    (x, y) match {
+      case (None, None) => ""
+      case (Some(x), None) => yearformat.print(x)
+      case (None, Some(y)) => yearformat.print(y)
+      case (Some(x), Some(y)) => {
+        if (x == y) yearformat.print(x)
+        else {
+          if (x.year != y.year) {
+            yearformat.print(x) + " - " + yearformat.print(y)
+          }
+          else if (x.monthOfYear() != y.monthOfYear()) {
+            monthformat.print(x) + " - " + monthformat.print(y) + ", " + yearOnlyFormat.print(x)
+          }
+          else if (x.dayOfMonth() != y.dayOfMonth()) {
+            dayformat.print(x) + " - " + dayformat.print(y) + ", " + monthAndYearFormat.print(x)
+          }
+          else yearformat.print(x)
+        }
+      }
+    }
+  }
 }
 
