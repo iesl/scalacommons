17669e5dfeb2750e68d86b55caad7b9bd57dc48f
Adam Saunders <saunders@cs.umass.edu>
2014-09-04 10:46:16 -0400

    release plugin

--- project/Build.scala
+++ project/Build.scala
@@ -1,55 +1,63 @@
 import sbt._
 import Keys._
-import edu.umass.cs.iesl.sbtbase.Dependencies
-import edu.umass.cs.iesl.sbtbase.IeslProject._
+import sbtrelease.ReleasePlugin._
+
+//import edu.umass.cs.iesl.sbtbase.Dependencies
+//import edu.umass.cs.iesl.sbtbase.IeslProject._
 
 
 object ScalaCommonsBuild extends Build {
 
   val vers = "0.2-SNAPSHOT"
 
-  implicit val allDeps: Dependencies = new Dependencies();
+  //implicit val allDeps: Dependencies = new Dependencies();
 
-  import allDeps._
+  //import allDeps._
+  val scalaV = "2.11.2"
 
   val deps = Seq(
-    //dsutils(),
-    "com.davidsoergel" % "dsutils" % "1.05",
-    scalatime(),
-    //def jodatime(v: String = "latest.release") = "joda-time" % "joda-time" % v applyGlobal()
-    // def scalatime(v: String = "latest.release") = "com.github.nscala-time" %% "nscala-time" % v applyGlobal()
-    
-    "commons-io" % "commons-io" % "2.4",
-    classutil(),
-    scalaCompiler("2.11.2"),
+
     // scalatest(),
-    "org.specs2" %% "specs2" % "2.4.1" % "test",
     //specs2("2.4.1"),
+
+    "com.github.nscala-time" %% "nscala-time" % "latest.release",
+    "org.clapper" %% "classutil" % "latest.release",
+    "org.scala-lang"                % "scala-compiler" % scalaV,
+    "com.davidsoergel"              % "dsutils" % "1.05",
+    "org.specs2"                    %% "specs2" % "2.4.1" % "test",
+    "commons-io"                    % "commons-io" % "2.4",
     "com.github.scala-incubator.io" %% "scala-io-core" % "latest.release",
     "com.github.scala-incubator.io" %% "scala-io-file" % "latest.release",
-
-    "com.typesafe" % "config" % "latest.release",
-    "org.jdom" % "jdom" % "1.1.3",
-    "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
-
-    //"com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
-
-    "ch.qos.logback" % "logback-classic"      % "latest.release",
-    "ch.qos.logback" % "logback-core"         % "latest.release",
-    "org.slf4j"      % "slf4j-api"            % "latest.release",
-    //"com.typesafe"   %% "scalalogging-slf4j"  % "latest.release",
-    "com.typesafe.scala-logging" %% "scala-logging" % "latest.release",
-    "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "latest.release",
-    "org.slf4j"      % "jcl-over-slf4j"       % "latest.release",
-    "org.slf4j"      % "log4j-over-slf4j"     % "latest.release",
-    "org.slf4j"      % "jul-to-slf4j"         % "latest.release",
-    "org.clapper"    %% "grizzled-slf4j"      % "latest.release"
+    "com.typesafe"                  % "config" % "latest.release",
+    "org.jdom"                      % "jdom" % "1.1.3",
+    "org.scala-lang.modules"        %% "scala-xml" % "1.0.1",
+    "ch.qos.logback"                % "logback-classic"      % "latest.release",
+    "ch.qos.logback"                % "logback-core"         % "latest.release",
+    "org.slf4j"                     % "slf4j-api"            % "latest.release",
+    //"com.typesafe"                %% "scalalogging-slf4j"  % "latest.release",
+    "com.typesafe.scala-logging"    %% "scala-logging" % "latest.release",
+    "com.typesafe.scala-logging"    %% "scala-logging-slf4j" % "latest.release",
+    "org.slf4j"                     % "jcl-over-slf4j"       % "latest.release",
+    "org.slf4j"                     % "log4j-over-slf4j"     % "latest.release",
+    "org.slf4j"                     % "jul-to-slf4j"         % "latest.release",
+    "org.clapper"                   %% "grizzled-slf4j"      % "latest.release"
   )
 
-  lazy val scalacommons = Project("scalacommons", file(".")) // .ieslSetup(
+  // TODO undo this copypasta from iesl sbt base for deps, logging config
+  lazy val scalacommons = Project("scalacommons", file("."))
+  .settings(releaseSettings:_*)
   .settings(
+    resolvers += "IESL Public Releases" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public",
     scalaVersion := "2.11.2",
-    //scalacOptions ++= Seq("-feature", "-deprecation"),
+    scalacOptions := Seq(
+      "-Xlint", "-deprecation", "-unchecked", "-Xcheckinit",
+      "-g:vars",
+      "-encoding", "utf8",
+      "-feature",
+      "-language:reflectiveCalls",
+      "-language:implicitConversions", "-language:postfixOps"
+    ),
+    javacOptions ++= Seq("-Xlint:unchecked", "-encoding", "utf8"),
     crossScalaVersions := Seq("2.10.4", "2.11.2"),
     libraryDependencies ++= deps,
     libraryDependencies ++= (
@@ -63,38 +71,3 @@ object ScalaCommonsBuild extends Build {
 
 }
 
-
-
-//class CleanLogging(deps: Dependencies) {
-// 
-//  import deps._
-// 
-//  def standardLogging(slf4jVersion:String="latest.release") = Seq(
-//    // see http://www.slf4j.org/legacy.html
-// 
-//    // ultimately log everything via Logback
-// 
-//    logbackCore(),
-//    logbackClassic(),
-// 
-//    // use the slf4j wrapper API
-//    slf4j(slf4jVersion),
-// 
-//    // nice Scala syntax for slf4j
-//    //slf4s(),
-//    scalalogging(),
-// 
-//    // direct legacy Jakarta Commons Logging calls to slf4j
-//    jclOverSlf4j(slf4jVersion),
-// 
-//    // direct legacy log4j calls to slf4j
-//    log4jOverSlf4j(slf4jVersion),
-// 
-//    // direct legacy java.util.logging calls to slf4j
-//    julToSlf4j(slf4jVersion),
-// 
-//    // direct grizzled-slf4j calls to slf4j
-//    grizzledSlf4j(slf4jVersion)
-//  )
-// 
-//}
