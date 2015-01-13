4ea9df68dfa09a680046d943f100349a63e6af08
Adam Saunders <saunders@cs.umass.edu>
2015-01-13 14:52:34 -0500

    fixed tests

--- src/test/scala/edu/umass/cs/iesl/scalacommons/collections/PrioritySetTest.scala
+++ src/test/scala/edu/umass/cs/iesl/scalacommons/collections/PrioritySetTest.scala
@@ -1,13 +1,13 @@
 package edu.umass.cs.iesl.scalacommons.collections
 
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Matchers
 import org.scalatest.FunSpec
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
  */
-class PrioritySetTest extends FunSpec with ShouldMatchers {
+class PrioritySetTest extends FunSpec with Matchers {
 
   describe("A priority set of integers") {
     val ord = new Ordering[Int] {
@@ -18,13 +18,13 @@ class PrioritySetTest extends FunSpec with ShouldMatchers {
     ps += 6 += 8 += 8 += 3 += 4 += 9 += 2 += 3 += 9 += 1 += 5 += 7
 
     it("drops duplicates") {
-      ps.toSet should be === Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
+      ps.toSet shouldEqual Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
     it("returns the largest element first") {
-      ps.dequeue() should be === 9
+      ps.dequeue() shouldEqual 9
     }
     it("returns remaining elements in the correct order") {
-      ps.dequeueAll should be === IndexedSeq(8, 7, 6, 5, 4, 3, 2, 1)
+      ps.dequeueAll shouldEqual IndexedSeq(8, 7, 6, 5, 4, 3, 2, 1)
     }
   }
 
@@ -38,13 +38,13 @@ class PrioritySetTest extends FunSpec with ShouldMatchers {
     ps += 6 += 8 += 8 += 3 += 4 += 9 += 2 += 3 += 9 += 1 += 5 += 7
 
     it("drops duplicates") {
-      ps.toSet should be === Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
+      ps.toSet shouldEqual Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
     it("returns the smallest element first") {
-      ps.dequeue() should be === 1
+      ps.dequeue() shouldEqual 1
     }
     it("returns remaining elements in the correct order") {
-      ps.dequeueAll should be === IndexedSeq(2, 3, 4, 5, 6, 7, 8, 9)
+      ps.dequeueAll shouldEqual IndexedSeq(2, 3, 4, 5, 6, 7, 8, 9)
     }
   }
 }
